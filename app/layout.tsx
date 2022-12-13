47fe594e6546f576a4c726a263501370dd54f74d
ryohidaka <git@ryohidaka.jp>
2022-12-13 19:04:45 +0900

    feat: widjet.js 読み込みコンポーネントを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
+import "./globals.css";
+import TweetScript from "./components/TweetScript";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +13,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+
+        {/* Load Twitter Widjet Script */}
+        <TweetScript />
+      </body>
     </html>
-  )
+  );
 }
