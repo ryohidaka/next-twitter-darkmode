864785f94fd68335235b69d55666bbc8df77e513
ryohidaka <git@ryohidaka.jp>
2022-12-13 18:52:39 +0900

    feat: Twitterクライアント初期化処理を追加

---
+++ lib/tweet.ts
@@ -0,0 +1,8 @@
+import Twitter from "twitter";
+
+export const twitterClient = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY || "",
+  consumer_secret: process.env.CONSUMER_SECRET || "",
+  access_token_key: process.env.ACCESS_TOKEN_KEY || "",
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET || "",
+});
